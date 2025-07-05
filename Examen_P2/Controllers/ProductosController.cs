af0aed8c488051041c412c9f5836dec997ee0462
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-04 21:04:40 -0600

    Ha ha quedado el swagger, falta que jale bien los datos, me falto agregarlos en el Program.cs

---
+++ Examen_P2/Controllers/ProductosController.cs
@@ -0,0 +1,30 @@
+﻿using Examen_P2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examen_P2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : Controller
+    {
+        // Variables para el contexto
+        private readonly ParcialDbContext _baseDatos;
+        public ProductosController(ParcialDbContext baseDatos)
+        {
+            _baseDatos = baseDatos;
+        }
+
+
+        // Creamos le metodo GET devolver los datos:
+        [HttpGet]
+        [Route("Listar")]
+        public async Task<IActionResult> Lista()
+        {
+            var listaProductos = await _baseDatos.Products.ToListAsync();
+            return Ok(listaProductos);
+        }
+
+    }
+}
