5c590338446b3e10ff4416d3374d3ef37e6d5106
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-04 21:12:03 -0600

    Ya trae los datos

--- Examen_P2/Program.cs
+++ Examen_P2/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +7,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Activamos swagger para la previsualización de los EndPoints
 builder.Services.AddSwaggerGen();
 
+
+// 
+var connectionString = builder.Configuration.GetConnectionString("cadenaSQL");
+builder.Services.AddDbContext<Examen_P2.Models.ParcialDbContext>(options => options.UseSqlServer(connectionString));    
+
+
+// Definimos la nueva política de CORS para que la API sea accesible desde cualquier origen
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaApi", app =>
+    {
+        app.AllowAnyOrigin() // Permite cualquier origen
+            .AllowAnyMethod() // Permite cualquier método HTTP (GET, POST, PUT, DELETE, etc.)
+            .AllowAnyHeader(); // Permite cualquier encabezado
+    });
+});
+
+
+// 
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +41,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Activamos la núeva política
+app.UseCors("NuevaApi");
 
 app.UseHttpsRedirection();
 
